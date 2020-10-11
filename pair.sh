87cb23225b994030e759092bc6f42d71bf68d419
SAKSHIPATEL <patelsakshi121@gmail.com>
2020-10-11 12:39:57 +0530

    Added : singlet combination and calculated percentage

--- pair.sh
+++ pair.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-random=$((RANDOM%2))
+echo -e "enter the times to toss coin \c"
+read num
+Head=0
+Tail=0
 
-if [ $random -eq 1 ]
-then
-	 echo "head"
-else
-	echo "tail"
-fi
+for ((i=0; i<$num; i++))
+do
+	random=$((RANDOM%2))
+	if [ $random -eq 1 ]
+	then
+		 ((Head++))
+	else
+		 ((Tail++))
+	fi
+done
+
+declare -A Coins
+Coins[((Head))]=$Head
+Coins[((Tail))]=$Tail
+echo ${Coins[@]}
+
+h=$(($Head*100))
+headper=$(($h/$num))
+t=$(($Tail*100))
+tailper=$(($t/$num))
+echo "head % : $headper , tails % : $tailper "
